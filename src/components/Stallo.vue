eb79ab35a4ef56036600b46870bffd659fb57344
Malkit Sandhu <malkit@sandhu.com>
2021-02-10 15:44:16 +0100

    Sisteamto bug classi css sbagliate e aggiornamento automatico dei dati con tolleranza

--- src/components/Stallo.vue
+++ src/components/Stallo.vue
@@ -1,28 +1,33 @@
 <template>
-  <div class="stallo" @click="handleClik" ref="stallo">
-    {{ params.nome }} - {{ params.stato }}
-  </div>
+    <div
+        v-bind:class="['stallo', classeStato]"
+        @click="handleClik"
+        ref="stallo"
+    >
+        {{ params.nome }} - {{ params.stato }}
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Stallo',
-  props: ['params'],
-  data() {
-    return {};
-  },
-  mounted() {
-    // console.log(this.params.stato);
-    let stato = this.params.stato;
-    let classe = stato.replace(' ', '_').toLowerCase();
-    this.$refs.stallo.classList.add(classe);
-  },
-  computed: {},
-  methods: {
-    handleClik: function() {
-      this.$router.push({ name: 'detail', params: this.params });
+    name: 'Stallo',
+    props: ['params'],
+    data() {
+        return {};
+    },
+    mounted() {},
+    computed: {
+        classeStato: function () {
+            let stato = this.params.stato;
+            let classe = stato.replace(' ', '_').toLowerCase();
+            return classe;
+        }
+    },
+    methods: {
+        handleClik: function () {
+            this.$router.push({ name: 'detail', params: this.params });
+        }
     }
-  }
 };
 </script>
 
@@ -31,27 +36,27 @@ export default {
 @import '@/sass/colori';
 
 .stallo {
-  border: 1px solid gray;
+    border: 1px solid gray;
 }
 .error {
-  background-color: $error;
+    background-color: $error;
 }
 .waiting {
-  background-color: $waiting;
+    background-color: $waiting;
 }
 .end_ok {
-  background-color: $end_ok;
+    background-color: $end_ok;
 }
 .manual_stop {
-  background-color: $manual_stop;
+    background-color: $manual_stop;
 }
 .in_progress {
-  background-color: $in_progress;
+    background-color: $in_progress;
 }
 .safety_block {
-  background-color: $safety_block;
+    background-color: $safety_block;
 }
 .ready {
-  background-color: $ready;
+    background-color: $ready;
 }
 </style>
